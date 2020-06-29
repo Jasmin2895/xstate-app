47b7deb526f05ed70dc539b67361738869a6c950
buckleader <jasmin28virdi@gmail.com>
2020-06-30 00:50:19 +0530

    eslint and prettier fixes

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li>Todo A</li>
+      <li>Todo B</li>
+      <li>Todo C</li>
+    </ul>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'TodoList',
+  data() {
+    return {}
+  }
+}
+</script>
+<style></style>
