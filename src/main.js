613728f7ac93d12a6f16fdc314b36b31bbdd8d4b
buckleader <jasmin28virdi@gmail.com>
2020-07-23 00:29:51 +0530

    update vue version

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import { store } from '../src/store/todoActions'
 
-Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
